d8869ca2a90406dfb67c5e69d222d5d4b8107c58
Mohit Sharma <mohitss0011@gmail.com>
2025-02-01 00:25:37 +0530

    username optimize

--- client/src/components/userProfile.jsx
+++ client/src/components/userProfile.jsx
@@ -3,25 +3,28 @@ import callApi from "../utility/callApi"
 import Loader from "./Loader"
 import { useNavigate } from "react-router"
 const UserProfile = ({ onClick, openSidebar, overlayHandle }) => {
-    const [apiData, setApiData] = useState([])
-    const [loader, setLoader] = useState(true)
+    const [loader, setLoader] = useState(false)
     const Navigate = useNavigate()
     const [defaults, setDefaults] = useState({
-        username: ''
+        username: localStorage.getItem("username") || null
     })
 
     useEffect(() => {
-        callApi('/userProfile', 'GET', null, setLoader).then((data) => {
-            console.log('data--', data.username)
-            setApiData(data)
-            setDefaults(prev => ({
-                ...prev,
-                username: data.username
-            }))
-        })
+        if (!defaults.username) {
+            setLoader(true)
+            callApi('/userProfile', 'GET', null, setLoader).then((data) => {
+                setDefaults(prev => ({
+                    ...prev,
+                    username: data.username
+                }))
+                localStorage.setItem("username", data.username)
+            })
+        }
+        return
     }, [])
 
     const handlerLogout = () => {
+        localStorage.removeItem("username")
         callApi('/logout', 'POST', { username: "dummy" }, setLoader).then((data) => {
             if (data.auth === false) {
                 return Navigate(data.resPath)
