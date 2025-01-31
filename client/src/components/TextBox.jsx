7f6d6bab076a019ebc57cfc6907715adf78501ca
Mohit Sharma <mohitss0011@gmail.com>
2025-02-01 00:05:02 +0530

    eye icon fix

--- client/src/components/TextBox.jsx
+++ client/src/components/TextBox.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react"
 const TextBox = (props) => {
+    const [eyeIcon, setEyeIcon] = useState("fa-eye")
     const { type, value, name, className, onChange, placeholder, label, id } = props
     const handlePass = () => {
-        document.getElementById("passwordInput").type == "password"
-            ? document.getElementById("passwordInput").type = "text"
-            : document.getElementById("passwordInput").type = "password"
+        if (document.getElementById("passwordInput").type == "password") {
+            document.getElementById("passwordInput").type = "text"
+            setEyeIcon("fa-eye-slash")
+        }
+        else {
+            document.getElementById("passwordInput").type = "password"
+            setEyeIcon("fa-eye")
+        }
     }
     return (
         <>
             <div className="textBox">
                 <div className="container">
                     <input id={id} type={type} name={name} value={value} placeholder={placeholder} className={className} onChange={onChange} />
-                    {type === "password" && <i className="fa fa-eye" id="togglePassword" onClick={handlePass}></i>}
+                    {type === "password" && <i className={`fa ${eyeIcon}`} id="togglePassword" onClick={handlePass}></i>}
                     {label && <label htmlFor={id}>{label}</label>}
                 </div>
 
